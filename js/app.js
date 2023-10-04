0699eec651606765f6749c643c1dac99b6931558
chmielewskigianpiero <chmielewskigianpiero@gmail.com>
2023-10-04 14:26:14 +0100

    ADD - file project and README.md

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// # BIGLIETTO DEL TRENO
+
+// - Chiedere all'utente quanti chilometri vuole percorrere
+//     - dichiarare una variabile dove salvare il nome
+//     - tramite un prompt assegnare il nome dell'utente alla variabile
+// - Chiedere all'utente l'età
+//     - dichiarare una variabile dove salvare l'età
+//     - tramite un prompt assegnare l'età alla variabile
+// - Calcolare il prezzo totale del viaggio definito sul totale dei chilometri (0,21)
+//     - SE l'utente ha meno di 18 anni
+//         - applicare uno sconto del 20%
+//     - ALTRIMENTI SE l'utente ha piu di 65 di anni
+//         - applicare uno sconto del 40%
+//     - ALTRIMENTI
+//         - non applicare nessuno sconto
