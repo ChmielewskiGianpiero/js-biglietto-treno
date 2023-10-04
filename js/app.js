e35eefae3d4e879241ad6b9c32621262d7b83c66
chmielewskigianpiero <chmielewskigianpiero@gmail.com>
2023-10-04 18:17:04 +0100

    ADD - basicPrice, discount and finalPrice html

--- js/app.js
+++ js/app.js
@@ -6,14 +6,39 @@
 let userKm = prompt ("Quanti chilometri vuoi percorrere?")
 userKm = parseInt(userKm)
 console.log (userKm)
-//     - tramite un prompt assegnare il nome dell'utente alla variabile
 // - Chiedere all'utente l'età
 //     - dichiarare una variabile dove salvare l'età
 //     - tramite un prompt assegnare l'età alla variabile
-// - Calcolare il prezzo totale del viaggio definito sul totale dei chilometri (0,21)
+let userAge = prompt ("Quanti anni hai?")
+userAge = parseInt(userAge)
+console.log (userAge)
+// - Calcolare il prezzo totale del viaggio definito sul totale dei chilometri (0,21 al km)
+//     - calcolare il prezzo base
+let basicPrice = userKm * 0.21
+console.log (basicPrice)
 //     - SE l'utente ha meno di 18 anni
 //         - applicare uno sconto del 20%
+//         - calcolare il prezzo scontato
+if(userAge < 18){
+    let discount = basicPrice * (20 / 100);
+    basicPrice = basicPrice - discount
+    console.log(discount, basicPrice);
+} else if (userAge > 65){
 //     - ALTRIMENTI SE l'utente ha piu di 65 di anni
 //         - applicare uno sconto del 40%
-//     - ALTRIMENTI
-//         - non applicare nessuno sconto
+//         - calcolare il prezzo scontato
+    discount = basicPrice * (40 / 100)
+    basicPrice = basicPrice - discount
+    console.log (discount, basicPrice)
+}
+
+// - stampare il prezzo finale nella pagina
+//      - recuperare dal DOM un elemento in base all'id:
+//      - dichiarare una variabile e tramite getElementById assegnargli il valore
+const finalPriceDomElement = document.getElementById('finalPrice')
+console.log(finalPriceDomElement)
+//   - modifcare l'innerHTML con il valore di finalPrice
+finalPriceDomElement.innerHTML = basicPrice
+//   - rendere l'output del prezzo con massimo 2 decimali
+
+
